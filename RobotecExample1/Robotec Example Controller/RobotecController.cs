da043726508ebb44f2eea46c508827c5f3125718
Rutherfordum <amaterasu.dzen@gmail.com>
2023-02-16 14:32:21 +0300

    init

--- RobotecExample1/Robotec Example Controller/RobotecController.cs
+++ RobotecExample1/Robotec Example Controller/RobotecController.cs
@@ -19,7 +19,14 @@ namespace RobotecExample
     {
         private readonly MainWindow _view;
 
-        private OpcClient _client;
+        private List<OpcClient> _clients = new List<OpcClient>()
+        {
+            new OpcClient("172.31.1.147:4840","OpcUaOperator", "kuka"), // 1 робот
+            new OpcClient("172.31.1.148:4840","OpcUaOperator", "kuka"), // 2 робот
+            new OpcClient("172.31.1.149:4840","OpcUaOperator", "kuka"), // 3 робот
+            new OpcClient("172.31.1.150:4840","OpcUaOperator", "kuka")  // 4 робот
+        };
+
         private Parameters _parameters;
         private Transform _manualTransform;
         private Transform _currentTransform;
@@ -70,34 +77,16 @@ namespace RobotecExample
         {
             try
             {
-                if (_client != null)
-                    _client?.Disconnect();
-
-                switch (_view.GetSelectedRobot())
-                {
-                    case 1:
-                        _client = new OpcClient("172.31.1.147:4840", "OpcUaOperator",
-                            "kuka");
-                        break;
-
-                    case 2:
-                        break;
-
-                    case 3:
-                        break;
-
-                    case 4:
-                        break;
-                }
+                _clients?.ForEach(client => client.Disconnect());
 
                 await Task.Run(() =>
                 {
-                    _client.OnConnected += Connected;
-                    _client.OnDisconnected += Disconnected;
-                    _client.OnFaulted += Faulted;
+                    _clients.OnConnected += Connected;
+                    _clients.OnDisconnected += Disconnected;
+                    _clients.OnFaulted += Faulted;
 
-                    if (_client != null)
-                        _client?.Connect();
+                    if (_clients != null)
+                        _clients?.Connect();
                 });
             }
             catch (Exception e)
@@ -108,7 +97,7 @@ namespace RobotecExample
 
         private void Disconnect()
         {
-            _client?.Disconnect();
+            _clients?.Disconnect();
             _view.SetConnectStatus(false);
         }
 
@@ -135,7 +124,7 @@ namespace RobotecExample
             _view.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
                 (ThreadStart)delegate () { _view.SetConnectStatus(false); });
 
-            Robot.Stop(_client);
+            Robot.Stop(_clients);
         }
 
         #endregion
@@ -144,9 +133,9 @@ namespace RobotecExample
 
         private async void StartTask()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
             GetDataFromView();
-            await Robot.SendParameters(_client, _parameters);
+            await Robot.SendParameters(_clients, _parameters);
 
             Enum_PG_TASK task = Enum_PG_TASK.NOTASK;
 
@@ -156,19 +145,19 @@ namespace RobotecExample
             if ((bool)_view.positionToggle.IsChecked)
                 task = Enum_PG_TASK.MAIN_POSITIONS;
 
-            await Measurement.Start(_client, task);
+            await Measurement.Start(_clients, task);
         }
 
         private async void StopTask()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
-            await Robot.Stop(_client);
+            await Robot.Stop(_clients);
         }
 
         private async void ContinueTask()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
             Enum_PG_TASK task = Enum_PG_TASK.NOTASK;
 
@@ -178,13 +167,13 @@ namespace RobotecExample
             if ((bool)_view.positionToggle.IsChecked)
                 task = Enum_PG_TASK.MAIN_POSITIONS;
 
-            await Measurement.Continue(_client, task);
+            await Measurement.Continue(_clients, task);
         }
 
         private async void EndMeasurement()
         {
-            if (_client == null) return;
-            await Measurement.Finish(_client);
+            if (_clients == null) return;
+            await Measurement.Finish(_clients);
         }
 
         #endregion
@@ -193,43 +182,43 @@ namespace RobotecExample
 
         private async void PositionHome()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
-            await Robot.GoToHome(_client);
+            await Robot.GoToHome(_clients);
         }
         private async void ResetData()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
-            await Robotec.Control.ManualControl.SendTransform(_client, new Transform());
-            await Robot.ResetParameters(_client);
+            await Robotec.Control.ManualControl.SendTransform(_clients, new Transform());
+            await Robot.ResetParameters(_clients);
             ResetViewParameters();
         }
         private async void ResetError()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
-            await _client.WriteNodeAsync(RWNodeData.R_START, true);
+            await _clients.WriteNodeAsync(RWNodeData.R_START, true);
             await Task.Delay(500);
-            await _client.WriteNodeAsync(RWNodeData.R_START, false);
+            await _clients.WriteNodeAsync(RWNodeData.R_START, false);
 
         }
         private async void PauseContinue()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
-            bool isPause = (bool)await _client.ReadNodeAsync(RWNodeData.R_HOLD);
+            bool isPause = (bool)await _clients.ReadNodeAsync(RWNodeData.R_HOLD);
 
             if (!isPause)
-                await Robot.Pause(_client);
+                await Robot.Pause(_clients);
             else
-                await Robot.Continue(_client);
+                await Robot.Continue(_clients);
         }
         private async void EmergencyStop()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
-            await Robot.EmergencyStop(_client);
+            await Robot.EmergencyStop(_clients);
         }
 
         #endregion
@@ -238,16 +227,16 @@ namespace RobotecExample
 
         public async void OpenGripper()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
-            await GripperControl.Open(_client);
+            await GripperControl.Open(_clients);
         }
 
         public async void CloseGripper()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
-            await GripperControl.Close(_client);
+            await GripperControl.Close(_clients);
         }
 
         #endregion
@@ -256,20 +245,20 @@ namespace RobotecExample
 
         private async void ManualControl()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
             GetDataFromView();
-            await Robotec.Control.ManualControl.SendTransform(_client, _manualTransform);
-            await Robotec.Control.ManualControl.Enable(_client);
+            await Robotec.Control.ManualControl.SendTransform(_clients, _manualTransform);
+            await Robotec.Control.ManualControl.Enable(_clients);
         }
 
         private async void StartMovingManual()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
             GetDataFromView();
-            await Robotec.Control.ManualControl.SendTransform(_client, _manualTransform);
-            await Robotec.Control.ManualControl.StartRobotMovement(_client);
+            await Robotec.Control.ManualControl.SendTransform(_clients, _manualTransform);
+            await Robotec.Control.ManualControl.StartRobotMovement(_clients);
         }
 
         #endregion
@@ -278,13 +267,13 @@ namespace RobotecExample
 
         private void Update()
         {
-            if (_client == null) return;
+            if (_clients == null) return;
 
             OutMessageAlarm();
-            while (_client.Session.State == CommunicationState.Opened)
+            while (_clients.Session.State == CommunicationState.Opened)
             {
-                _currentTransform = Robot.GetActualTransformRobot(_client);
-                _targetTransform = Robot.GetTargetTransformRobot(_client);
+                _currentTransform = Robot.GetActualTransformRobot(_clients);
+                _targetTransform = Robot.GetTargetTransformRobot(_clients);
                 Task.Delay(500);
                 TransformDataToView();
             }
@@ -306,10 +295,10 @@ namespace RobotecExample
         {
             try
             {
-                while (_client.Session.State == CommunicationState.Opened)
+                while (_clients.Session.State == CommunicationState.Opened)
                 {
-                    string mes = (string) await _client.ReadNodeAsync(RNodeData.OA_MESSAGE) ?? "Disconnected";
-                    
+                    string mes = (string)await _clients.ReadNodeAsync(RNodeData.OA_MESSAGE) ?? "Disconnected";
+
                     switch (mes)
                     {
                         case "Disconnected":
